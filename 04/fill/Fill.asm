1556f59b1e2c6b818b619a37affb2803c270ac3a
Julian Fitzell <julian.fitzell@harpercollins.co.uk>
2016-03-07 18:14:02 +0000

    Implement Fill.asm in Project 4 - draw one word every loop that a key is pressed

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,43 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(INIT)
+	@SCREEN
+	D=A
+	@word
+	M=D // set pixel to the screen buffer
+
+(LOOP)
+	@KBD
+	D=M  // get current key
+	@KEY
+	D;JNE
+	
+(NOKEY)
+	@0
+	D=A
+	@OUTPUT
+	0;JMP
+
+(KEY)
+	@0
+	D=!A
+	
+(OUTPUT)
+	@word
+	A=M
+	M=D
+
+(INC)	
+	@word
+	MD=M+1
+	@SCREEN
+	D=D-A
+	@8192
+	D=D-A
+	@INIT
+	D;JEQ
+	@LOOP
+	0;JMP
+	
+	
